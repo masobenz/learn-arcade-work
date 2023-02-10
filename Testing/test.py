6fbc31da6495319a78a4422be6962e4dfeed642a
masobenz <112599664+masobenz@users.noreply.github.com>
2023-02-09 21:05:11 -0700

    complete trees

--- Testing/test.py
+++ Testing/test.py
@@ -20,8 +20,16 @@ arcade.draw_ellipse_filled(200,370,60,80, arcade.csscolor.DARK_GREEN)
 
 #tree with arc top
 arcade.draw_rectangle_filled(300,320,20,60, arcade.csscolor.SIENNA)
-arcade.draw_arc_filled(300,340,60,100, arcade.csscolor.DARK_GREEN)
+arcade.draw_arc_filled(300,340,60,100, arcade.csscolor.DARK_GREEN, 0,180)
+
+# tree with triangle top
+arcade.draw_rectangle_filled(400,320,20,60, arcade.csscolor.SIENNA)
+arcade.draw_triangle_filled(400,400,370,320,430,320, arcade.csscolor.DARK_GREEN)
+
+# tree with polygon top
+arcade.draw_rectangle_filled(500,320,20,60, arcade.csscolor.SIENNA)
+arcade.draw_polygon_filled(((500,400),(480,360),(470,320),(530,320),(520,360)), arcade.csscolor.DARK_GREEN)
 
 arcade.finish_render()
 
-arcade.run()  
+arcade.run()
